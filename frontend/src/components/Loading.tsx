9aa02dffe2cec29d42855384c225b5354c0c0cd9
barrfalk <bfalk@salussystems.com>
2025-05-15 12:03:53 -0700

    More memory limit updates

--- frontend/src/components/Loading.tsx
+++ frontend/src/components/Loading.tsx
@@ -6,7 +6,6 @@ export default function Loading(props: any) {
   return (
     <Backdrop
       sx={{
-        color: "#fff",
         zIndex: (theme) => theme.zIndex.drawer + 1,
         display: "flex",
         flexDirection: "column",

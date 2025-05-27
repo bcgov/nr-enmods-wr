860f06b338a77e9add64dff0c3b43c6f77324748
mgtennant <100305096+mgtennant@users.noreply.github.com>
2025-05-27 11:31:32 -0700

    adding workflows back in

--- migrations/sql/V1.0.0__file_info.sql
+++ migrations/sql/V1.0.0__file_info.sql
@@ -2,6 +2,6 @@ CREATE TABLE file_info (
   id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
   file_name VARCHAR(100) NOT NULL,
   date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-)
+);
 
-COMMENT ON TABLE file_info IS 'Stores full file name as well as creation date, new files are created by upserting the most recent file.'
+COMMENT ON TABLE file_info IS 'Stores full file name as well as creation date, new files are created by upserting the most recent file.';

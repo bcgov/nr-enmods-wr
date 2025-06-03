aeea7079d77ae967dd33bb08cebc0e8e7fb0c0fe
mgtennant <100305096+mgtennant@users.noreply.github.com>
2025-06-03 11:55:37 -0700

    Update R__test_data.sql

--- migrations/sql/R__test_data.sql
+++ migrations/sql/R__test_data.sql
@@ -1,3 +1,3 @@
-INSERT INTO file_info (id, file_name, date_created) 
-VALUES ('0d205adb-5989-436c-bcad-62520c8c29f3', 'sampling_locations_06_03_2025__08_57_40.gpkg', '2025-06-03 00:00:00') 
-ON CONFLICT DO NOTHING;
+-- INSERT INTO file_info (id, file_name, date_created) 
+-- VALUES ('0d205adb-5989-436c-bcad-62520c8c29f3', 'sampling_locations_06_03_2025__08_57_40.gpkg', '2025-06-03 00:00:00') 
+-- ON CONFLICT DO NOTHING;
